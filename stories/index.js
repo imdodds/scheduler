3d1942213eea6b62de77899cebb85601e4712b09
imdodds <ianmitchelldodds@gmail.com>
2022-11-07 10:17:06 -0500

    Build InterviewerListItem component

--- stories/index.js
+++ stories/index.js
@@ -8,7 +8,10 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+import InterviewerListItem from "components/InterviewerListItem";
 
+
+// Button component tests
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -26,6 +29,7 @@ storiesOf("Button", module)
   ));
 
 
+// DayListItem component tests
 storiesOf("DayListItem", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -37,6 +41,9 @@ storiesOf("DayListItem", module)
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
   ));
   
+
+// DayList component tests
+
 const days = [
   {
     id: 1,
@@ -68,3 +75,41 @@ storiesOf("DayList", module)
   .add("Wednesday", () => (
       <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
   ));
+
+
+// InterviewerListItem component tests
+
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
+  
