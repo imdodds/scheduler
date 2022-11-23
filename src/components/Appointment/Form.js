af7984672358beb0928249b1e29862ff80f86c72
imdodds <ianmitchelldodds@gmail.com>
2022-11-23 18:59:18 -0500

    Set up testing

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,6 +6,7 @@ export default function Form(props) {
 
   const [student, setStudent] = useState(props.student || "")
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   function reset() {
     setStudent("");
@@ -15,6 +16,21 @@ export default function Form(props) {
   function cancel() {
     reset();
     props.onCancel();
+  }
+
+  function validate() {
+
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+
+    props.onSave(student, interviewer);
   }
 
   return (
@@ -28,19 +44,28 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
-          interviewer={props.interviewer}
+          // interviewer={props.interviewer}
           value={interviewer}
           onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={() => cancel()}>Cancel</Button>
-          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
+          <Button danger 
+            onClick={() => {
+              cancel();
+              reset();
+            }}>Cancel</Button>
+          <Button confirm 
+            onClick={() => {
+              validate() && props.onSave(student, interviewer);
+            }}>Save</Button>
         </section>
       </section>
     </main>
